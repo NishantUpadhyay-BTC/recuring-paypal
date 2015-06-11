346b649810d9b165be7cbe7d8e6e822396ab1e3a
Nishant Upadhyay <nishant.upadhyay@beaverlogic.com>
2015-06-11 16:38:58 +0530

    change url to post for sandbox account

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -81,15 +81,14 @@ class PaypalController < ApplicationController
 
   protected
   def validate_IPN_notification(raw)
-    uri = URI.parse('https://www.paypal.com/cgi-bin/webscr?cmd=_notify-validate')
+    uri = URI.parse('https://www.sandbox.paypal.com/cgi-bin/webscr')
     http = Net::HTTP.new(uri.host, uri.port)
     http.open_timeout = 60
     http.read_timeout = 60
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     http.use_ssl = true
     response = http.post(uri.request_uri, raw,
-                         'Content-Length' => "#{raw.size}",
-                         'User-Agent' => "My custom user agent"
+                         'Content-Length' => "#{raw.size}"
     )
     puts response.body
   end
